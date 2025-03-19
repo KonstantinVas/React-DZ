370987797f4f73825c972985c3e66b3f990591aa
KonstantinVas <110903969+KonstantinVas@users.noreply.github.com>
2025-03-19 13:46:38 +0300

    1-start Верства карточек и списка

--- 1-start/dz-project/src/components/Button/Button.jsx
+++ 1-start/dz-project/src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import './Button.css';
 
-function Button() {
+function Button({text, onClick}) {
 	return (
-		<button className="button">Искать</button>
+		<button className="button" onClick={onClick}>{text}</button>
 	);
 }
 
